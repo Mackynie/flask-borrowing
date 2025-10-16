d48a8f992f0fecad0cb581522feaedcb67a8bf69
Mackynie <143497149+Mackynie@users.noreply.github.com>
2025-10-16 11:55:27 +0800

    Update app.py

--- admin_web/app.py
+++ admin_web/app.py
@@ -200,7 +200,7 @@ class AdminActivity(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     admin_id = db.Column(db.Integer, db.ForeignKey('admins.id'), nullable=False)
     action = db.Column(db.Text, nullable=False)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    timestamp = db.Column(db.DateTime, default=datetime.cnow)
 
     admin = db.relationship('Admin', backref='activities')
 
